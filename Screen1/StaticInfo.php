0f17d218fcfb833b3832d5c75814b153db0d5475
JOTAI <juan.basflo.initec@gmail.com>
2016-06-02 05:56:32 -0500

    Scripst de Screen 1

---
+++ Screen1/StaticInfo.php
@@ -0,0 +1,23 @@
+<?php
+// Static Info to Load Screen 1
+
+$StaticInfo = array(
+    'HiUser' => 'Hola Emilio',
+    'NumDanger'=> 1,
+    'NumRisk'=> 3,
+    'MonitorBlock' => [
+        ['Id'=>1,
+         'Name' => 'Planta de Purificacion de Agua',
+         'CodeName'=> 'Bloque-101',
+         'NumMonitorPoints'=> 5],
+        ['Id'=>2,
+         'Name' => 'Tanque XYZ',
+         'CodeName'=> 'Bloque-102',
+         'NumMonitorPoints'=> 3]
+    ]
+    
+    // Maybe I could include Alerts
+);
+
+echo json_encode($StaticInfo);
+?>
